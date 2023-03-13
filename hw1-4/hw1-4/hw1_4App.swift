ecb27fbcd46052cf12aac83c0b7046f1fede488a
elisa403 <96239099+elisa403@users.noreply.github.com>
2023-03-13 22:22:02 +0800

    Add files via upload

---
+++ hw1-4/hw1-4/hw1_4App.swift
@@ -0,0 +1,17 @@
+//
+//  hw1_4App.swift
+//  hw1-4
+//
+//  Created by Elisa Chien on 2023/3/13.
+//
+
+import SwiftUI
+
+@main
+struct hw1_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
